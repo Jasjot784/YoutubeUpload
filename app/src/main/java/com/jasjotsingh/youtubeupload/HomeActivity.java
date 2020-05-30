5bf238b66420df5a2121434f6a7c67d7164595e7
Jasjot Singh <jasjot784@gmail.com>
2020-05-30 22:32:55 +0530

    Dependencies api key and Oauth completed for youtube api

---
+++ app/src/main/java/com/jasjotsingh/youtubeupload/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.jasjotsingh.youtubeupload;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jasjotsingh.youtubeupload.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
